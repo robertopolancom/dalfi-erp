9cd553ce996e303dd852cc78cd7ead342d470a4e
robertopolancom <robertopolanco03@gmail.com>
2026-08-13 19:10:20 -0400

    feat: add ReservApp accounts, multi-service booking and team agenda

--- outputs/reservar/sw.js
+++ outputs/reservar/sw.js
@@ -1,4 +1,4 @@
-const CACHE="dalfi-reservas-v1";
+const CACHE="dalfi-reservas-v2";
 const ASSETS=["./","./styles.css","./app.js","./config.js"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
